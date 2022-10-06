f470baeb62b954fce01f6f563cb2f159a29c10e7
Sergey Beketov <srgbeketov@gmail.com>
2022-10-06 12:57:57 +0500

    Исправил ошибки в функции. Добавил сортировку в алфавитном порядке.

--- lesson1/max-item-association.js
+++ lesson1/max-item-association.js
@@ -1,41 +1,60 @@
 function  maxItemAssociation(transactions) {
 
     // Составляем таблицу частоты появления товара
-    let frequencies = []
+    let items = []
     transactions.forEach(transaction => {
         transaction.forEach(item => {
-            let found = frequencies.find(f => f.name === item)
+            let found = items.find(i => i.name === item)
             if (found) {
-                found.count++
+                found.frequency++
                 transaction.forEach(t => {
-                    if (!found.associations.includes(t)) found.associations.push(t)
+                    if (!found.associationItems.includes(t)) found.associationItems.push(t)
                 })
             } else {
-                frequencies.push({name: item, count: 1, associations: transaction})
+                items.push({name: item, frequency: 1, associationItems: transaction.slice(0)})
             }
         })
     })
 
-    // Убираем из таблицы значения покупок меньше 2 и сортируем в порядке убывания
-    frequencies = frequencies
-        .filter(f => f.count > 1)
-        .sort((a, b) => b.count - a.count)
+    // Убираем из таблицы значения покупок меньше 2
+    items = items
+        .filter(f => f.frequency > 1)
 
-    // Выделяем первую максимальную ассоциацию из списка
-    return frequencies[0] ? frequencies[0].associations.sort() : []
+    // Состовляем все возможные ассоциациативные ряды
+    let associations = []
+    transactions.forEach(transaction => {
+        let association = []
+        items.forEach(item => {
+            if(transaction.includes(item.name)) {
+                item.associationItems.forEach(ai => {
+                    if(!association.includes(ai)) association.push(ai)
+                })
+            }
+        })
+        if(association.length > 0) associations.push(association.sort())
+    })
+
+    // Сортируем ассоциации по длинне и в алфавитном порядке
+    associations.sort((a, b) => {
+        if(b.length === a.length) return a.join('') > b.join('') ? 1 : -1
+        return b.length - a.length
+    })
+
+    return associations[0]
 }
 
 
 // Тесты
 const transactions = [
-    ['q', 'w', 'a'],
     ['a', 'b'],
     ['a', 'c'],
     ['q', 'e'],
     ['q', 'r'],
+    ['q', 'w', 'a'],
 ]
 
 const transactions2 = [
+    ['e', 'm'],
     ['a', 'b'],
     ['a', 'c'],
     ['d', 'e'],
