02695ce598b2a32d67943d8b5557c02b2350169d
Sergey Beketov <srgbeketov@gmail.com>
2023-03-06 21:32:51 +0500

    Перевод проекта на Typescript

--- lesson20/ts/script.ts
+++ lesson20/ts/script.ts
@@ -1,31 +1,36 @@
 'use strict';
-const weatherBlock = document.querySelector('#weather');
-async function loadWeather() {
-    if (weatherBlock != null) {
+
+const weatherBlock: HTMLElement | null = document.querySelector('#weather');
+
+async function loadWeather () {
+    if(weatherBlock != null) {
         weatherBlock.innerHTML = `
         <div class="weather__loading">
             <img src="../img/loading.gif" alt="Loading...">
         </div>
     `;
-        const server = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=Ekaterinburg&appid=44e6c41e0a29aa7d0d17f585cd8845e3';
-        const response = await fetch(server, {
+
+            const server: string = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=Ekaterinburg&appid=44e6c41e0a29aa7d0d17f585cd8845e3';
+        const response: Response = await fetch(server, {
             method: 'GET',
         });
         const responseResult = await response.json();
+
         if (response.ok) {
             getWeather(responseResult);
-        }
-        else {
+        } else {
             weatherBlock.innerHTML = responseResult.message;
         }
     }
 }
-function getWeather(data) {
-    const location = data.name;
-    const temp = Math.round(data.main.temp);
-    const feelsLike = Math.round(data.main.feels_like);
-    const weatherStatus = data.weather[0].main;
-    const weatherIcon = data.weather[0].icon;
+
+function getWeather(data: { name: string; main: { temp: number; feels_like: number; }; weather: { main: string; icon: string; }[]; }) {
+    const location: string = data.name;
+    const temp: number = Math.round(data.main.temp);
+    const feelsLike: number = Math.round(data.main.feels_like);
+    const weatherStatus: string = data.weather[0].main;
+    const weatherIcon: string = data.weather[0].icon;
+
     const template = `
         <div class="weather__header">
             <div class="weather__main">
@@ -39,10 +44,12 @@ function getWeather(data) {
         <div class="weather__temp">${temp}</div>
         <div class="weather__feels-like">Feels-like: ${feelsLike}</div>
     `;
-    if (weatherBlock != null) {
+
+    if(weatherBlock != null) {
         weatherBlock.innerHTML = template;
     }
 }
+
 if (weatherBlock) {
     loadWeather();
 }
