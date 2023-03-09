71246a36826e705591ca767bfb51ab3659081662
Sergey Beketov <srgbeketov@gmail.com>
2023-03-09 08:07:32 +0500

    Пустой проект на React

---
+++ lesson28/weather-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h4>Hello, React</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
